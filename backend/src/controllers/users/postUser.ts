5684a61a7002107a33881fc5e19999e097408526
Your Name <you@example.com>
2021-09-26 17:03:23 +0200

    Fix user creation

--- backend/src/controllers/users/postUser.ts
+++ backend/src/controllers/users/postUser.ts
@@ -1,9 +1,11 @@
+import sha224 from 'crypto-js/sha224'
 import { Request, Response } from 'express'
 import { User } from '../../models/User'
 
 export async function postUser(req: Request, res: Response) {
   const user: User = req.body.user
   try {
+    if (user.password) user.password = sha224(user.password).toString()
     const result = await User.getRepository().save(user)
     res.status(200).json(result.id)
   } catch (error) {
